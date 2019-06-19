5e42e0e10918e7f298b7d42d69a65ccdceff2d99
ShirishaReddyDhanasi <ShirishaReddyDhanasi@DESKTOP-J8124DH.in.ibm.com>
2019-06-19 12:56:13 +0530

    querymapper

--- src/com/ibm/sf/dao/QueryMapper.java
+++ src/com/ibm/sf/dao/QueryMapper.java
@@ -1,5 +1,10 @@
 package com.ibm.sf.dao;
 
-public class QueryMapper {
-
+public interface QueryMapper {
+	public static final String LOG_IN="select * from login_mast where user_id=? and pwd=?";
+	public static final String CUST_REGISTER="insert into cust_register_mast values(?,?,?,?,?,?,?,?)";
+	public static final String CUST_TRANSACT="insert into transact_mast values(?,?,?,?,?,?)";
+	public static final String GET_IFSC="select * from ifsc_mast where branch_name=?";
+	public static final String GET_CURRENTAMOUNT="select * from acc_mast where reg_id=?";
+	
 }
