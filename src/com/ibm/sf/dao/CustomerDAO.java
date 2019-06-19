ae5b06edeacc779d0d27254c6e01f66cadf439a1
Surya Mitra <suryamitracse@gmail.com>
2019-06-19 09:38:50 +0530

    Dao

---
+++ src/com/ibm/sf/dao/CustomerDAO.java
@@ -0,0 +1,23 @@
+package com.ibm.sf.dao;
+
+import java.time.LocalDate;
+
+public interface CustomerDAO {
+
+	// login method
+	public abstract Integer login(String uid,String password);
+	
+	//current available amount
+	public abstract Double getCurrentAmount(String uid);
+	//get customer statement
+	public abstract String getStatement(String uid);
+	//funds transfer
+	public abstract Integer fundsTransfer(String name,Long accountno,Integer type,String ifsc,Double amount);
+	//mobile topup
+	public abstract Integer mobileTopUp(Long mobno,String operator,Double amount);
+	//customer registration
+	
+	public abstract Integer customerReg(String name,Long accountno,String email,LocalDate dob,String fname,Long mobile,String password);
+	
+
+}
